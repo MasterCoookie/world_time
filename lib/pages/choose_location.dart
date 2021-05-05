e7741c9a19a231a4efef32a381523ee025964090
Jan Kocurek <jas.kocurek@wp.pl>
2021-05-05 17:53:03 +0200

    http

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,26 +9,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-    //simulate network req for username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'Name';
-    });
-
-    //simulate using name in another req
-    String password = await Future.delayed(Duration(seconds: 2), () {
-      return 'Password';
-    });
-
-    print('$username - $password');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
