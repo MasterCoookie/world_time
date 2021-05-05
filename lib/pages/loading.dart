9f859ef10d12a9b5f034a45d07c632d32e996ae2
Jan Kocurek <jas.kocurek@wp.pl>
2021-05-05 18:16:15 +0200

    getTime()

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,15 +11,31 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Charset': 'utf-8'
+    };
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Europe/Warsaw'), headers: headers);
     Map data = jsonDecode(response.body);
+    // print(data);
+
+    //getting properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(datetime);
+    // print(offset);
+    
+    //creating DatTime
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
