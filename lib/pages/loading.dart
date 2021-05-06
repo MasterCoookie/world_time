21d96d6178384651afaf6877851bc9ebf044c252
Jan Kocurek <jas.kocurek@wp.pl>
2021-05-06 21:56:08 +0200

    world_time class + error handling

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key key}) : super(key: key);
@@ -11,37 +10,26 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    Map<String, String> headers = {
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Charset': 'utf-8'
-    };
-    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Europe/Warsaw'), headers: headers);
-    Map data = jsonDecode(response.body);
-    // print(data);
+  String time = 'loading';
 
-    //getting properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    // print(datetime);
-    // print(offset);
-    
-    //creating DatTime
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location_ui: 'Berlin', flag_url: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading Screen'),
+      body: Text(time),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location_ui; // location name for the UI
+  String time; // time in location
+  String flag_url; // url to asset flag icon
+  String url; // location url ending for API
+
+  WorldTime({ this.location_ui, this.flag_url, this.url });
+
+  Future<void> getTime() async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Charset': 'utf-8'
+    };
+    try {
+      Response response = await get(
+          Uri.parse('https://worldtimeapi.org/api/timezone/$url'),
+          headers: headers);
+      Map data = jsonDecode(response.body);
+      // print(data);
+
+      //getting properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      // print(datetime);
+      // print(offset);
+
+      //creating DateTime
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      // print(now);
+
+      time = now.toString();
+    }
+
+    catch (e) {
+      print('Error cought $e');
+      time = "Could not get time data";
+    }
+  }
+}
